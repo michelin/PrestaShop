fa6800e68474393fe19b8bef23a2a450d5c2827e
Houssem MAAMRIA <houssem.maamria_ext@michelin.com>
2026-02-23 08:36:55 +0000

    synchro branch with core

--- back/themes/default/js/bundle/product/form.js
+++ back/themes/default/js/bundle/product/form.js
@@ -94,6 +94,7 @@ function resetEditor() {
  * Manage show or hide fields
  */
 window.displayFieldsManager = (function () {
+  
   // const typeProduct = $('#form_step1_type_product');
   //$('.form_step1_type_product').css("visibility","hidden");
   const typeProduct = $('#form_step1_type_product').val(2);
